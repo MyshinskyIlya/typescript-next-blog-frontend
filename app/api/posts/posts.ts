cc7d961852a91cc6ef94411757ca246abf26f3d2
MyshinskyIlya <myshinskiyyy@gmail.com>
2023-11-24 22:45:57 +0400

    "upd posts"

--- app/api/posts/posts.ts
+++ app/api/posts/posts.ts
@@ -47,4 +47,16 @@ export const posts = [
         title: "dolorem dolore est ipsam",
         body: "dignissimos aperiam dolorem qui eum\nfacilis quibusdam animi sint suscipit qui sint possimus cum\nquaerat magni maiores excepturi\nipsam ut commodi dolor voluptatum modi aut vitae",
     },
+    {
+        userId: 1,
+        id: 9,
+        title: "nesciunt iure omnis dolorem tempora et accusantium",
+        body: "consectetur animi nesciunt iure dolore\nenim quia ad\nveniam autem ut quam aut nobis\net est aut quod aut provident voluptas autem voluptas",
+    },
+    {
+        userId: 1,
+        id: 10,
+        title: "optio molestias id quia eum",
+        body: "quo et expedita modi cum officia vel magni\ndoloribus qui repudiandae\nvero nisi sit\nquos veniam quod sed accusamus veritatis error",
+    },
 ];
